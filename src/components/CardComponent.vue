5a67a6cee4d91ec3a318cc8df70c37242c796150
Isrrael Bolivar <isrraelbolivar77@gmail.com>
2023-08-02 00:02:40 -0400

    Subiendo proyecto

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,359 @@
+<template>
+  <div class="container cont1 text-center">
+    <div class="row">
+      <div class="col initialsDate">M|I</div>
+    </div>
+    <div class="row">
+      <div class="col text-shipping-date text-weight-bolder">NUESTRA BODA</div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="container banner-principal">
+          <q-img src="~assets/img_photo4.jpg" />
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="text-shipping2 text-weight-bolder q-pa-md">
+            CON LA BENDICIÓN DE DIOS Y LA DE NUESTRAS FAMILIAS NOSOTROS
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="div names">María Esther</div>
+          <div class="div names">e</div>
+          <div class="div names padding-name">Isrrael Eduardo</div>
+
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="div text-shipping2 text-weight-bolder padd">
+            TENEMOS EL HONOR DE INVITARTE A NUESTRA BODA CIVIL
+          </div>
+          <div class="q-pt-md">
+            <div class="div text-shipping-date text-weight-bolder">
+              SEPTIEMBRE
+            </div>
+          </div>
+          <div class="row">
+            <div class="col text-shipping-date text-weight-bolder q-pt-md">
+              <q-separator color="green-10" />
+              SÁBADO
+              <q-separator color="green-10" />
+            </div>
+            <div class="col numberDate text-green-10">16</div>
+            <div class="col text-shipping-date text-weight-bolder q-pt-md">
+              <q-separator color="green-10" />
+              2023
+              <q-separator color="green-10" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container cont2">
+    <div class="row">
+      <div class="col-md">
+        <div class="card-benefits q-pa-md">
+          <q-item-section class="q-pt-md">
+            <div class="col text-center">
+              <q-avatar rounded>
+                <img src="~assets/geo-alt.svg" />
+              </q-avatar>
+            </div>
+          </q-item-section>
+          <q-card-section class="text-center">
+            <div class="text-subtitle text-weight-bolder">UBICACIÓN</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none text-center">
+            <div class="text-shipping-date q-px-md text-weight-bolder">
+              5:30 PM
+            </div>
+            <div class="text-shipping-date q-px-md text-weight-bolder">
+              EL GRAN MUELLE MACAGUA
+            </div>
+            <div class="text-shipping2 q-px-md text-weight-bolder">
+              ESTE ES UN DÍA MUY ESPECIAL PARA NOSOTROS Y NOS ENCANTARÍA QUE
+              PUDIERAN SER PARTE DE EL.
+            </div>
+          </q-card-section>
+          <q-card-actions vertical align="center">
+            <q-btn
+              class="btn"
+              unelevated
+              rounded
+              href="https://www.google.com/maps/place/Muelle+Macagua/@8.285646,-62.6681541,17z/data=!4m14!1m7!3m6!1s0x8dcbfb64505992b3:0xff807c48df1f015e!2sMuelle+Macagua!8m2!3d8.285646!4d-62.6659654!16s%2Fg%2F11ssxf5gnj!3m5!1s0x8dcbfb64505992b3:0xff807c48df1f015e!8m2!3d8.285646!4d-62.6659654!16s%2Fg%2F11ssxf5gnj?entry=ttu"
+              color="green-10"
+              >VER UBICACIÓN</q-btn
+            >
+          </q-card-actions>
+        </div>
+      </div>
+      <div class="col-md">
+        <div class="card-benefits q-pa-md">
+          <q-item-section class="q-pt-md">
+            <div class="col text-center">
+              <q-avatar rounded>
+                <img src="~assets/icon-gift.svg" />
+              </q-avatar>
+            </div>
+          </q-item-section>
+          <q-card-section class="text-center">
+            <div class="text-subtitle text-weight-bolder">
+              SUGERENCIA DE REGALO
+            </div>
+          </q-card-section>
+          <q-card-section class="q-pt-none text-center">
+            <div class="text-shipping2 q-px-md text-weight-bolder">
+              REGALO OBLIGATORIO: ¡SU PRESENCIA! PERO SI NOS QUIEREN REGALAR
+              ALGO PARA NUESTRA VIDA JUNTOS, ESTA ES LA OPCIÓN:
+            </div>
+            <div class="text-shipping-date q-px-md text-weight-bolder">
+              EFECTIVO EN SOBRE
+            </div>
+            <div class="col text-center">
+              <q-avatar rounded>
+                <img src="~assets/envelope.svg" />
+              </q-avatar>
+            </div>
+          </q-card-section>
+        </div>
+      </div>
+      <div class="col-md">
+        <div class="card-benefits q-pa-md">
+          <q-item-section class="q-pt-md">
+            <div class="col text-center">
+              <q-avatar rounded>
+                <img src="~assets/icon-whatsapp.svg" />
+              </q-avatar>
+            </div>
+          </q-item-section>
+          <q-card-section class="text-center">
+            <div class="text-subtitle text-weight-bolder">CONFIRMACIÓN</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none text-center">
+            <div class="text-shipping2 q-px-md text-weight-bolder">
+              AGRADECEMOS QUE CONFIRMES TU ASISTENCIA ANTES DEL
+              <div class="text-shipping-date text-weight-bolder">17/08/2023</div>
+            </div>
+          </q-card-section>
+          <q-card-actions vertical align="center">
+            <q-btn
+              href="https://wa.link/lzjy3j"
+              color="green-10"
+              unelevated
+              rounded
+              class="btn"
+              >ENVIAR MENSAJE</q-btn
+            >
+          </q-card-actions>
+        </div>
+      </div>
+      <div class="col-md">
+        <div class="card-benefits q-pa-md padding-card">
+          <q-item-section class="q-pt-md">
+            <div class="col text-center">
+              <q-avatar rounded>
+                <img src="~assets/dresscode.png" />
+              </q-avatar>
+            </div>
+          </q-item-section>
+          <q-card-section class="text-center">
+            <div class="text-subtitle text-weight-bolder">DRESSCODE</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none text-center">
+            <div class="text-shipping2 q-px-md text-weight-bolder">
+              CÓDIGO DE VESTIMENTA FORMAL.
+            </div>
+          </q-card-section>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col text-center">
+        <div class="div text-shipping-date text-weight-bolder">
+          ESPERAMOS CONTAR CON SU PRESENCIA
+        </div>
+        <div class="div text-shipping2 text-weight-bolder text-green-10">
+          ¡MUCHAS GRACIAS!
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="container q-pt-lg">
+          <q-img class="img2" src="~assets/img_photo2.jpg" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.btn {
+  font-family: "DMSans-Bold";
+}
+.title-about-shipping {
+  font-family: "DMSans-Bold";
+  font-size: 30px;
+}
+
+.text-subtitle {
+  font-family: "YsabeauInfant-Bold";
+  font-size: 20px;
+  color: #617f6b;
+}
+
+.text-shipping-date {
+  font-family: "YsabeauInfant-Bold";
+  font-size: 18px;
+  color: #bf930d;
+}
+
+.text-shipping {
+  font-family: "YsabeauInfant-Extralight";
+  font-size: 18px;
+  color: #bf930d;
+}
+
+.text-shipping2 {
+  font-family: "YsabeauInfant-Extralight";
+  font-size: 18px;
+}
+
+.names {
+  font-family: "GreatVibes-Regular";
+  font-size: 45px;
+  color: #617f6b;
+}
+
+.numberDate {
+  font-family: "PurplePurse-Regular";
+  font-size: 50px;
+}
+
+.initialsDate {
+  font-family: "PurplePurse-Regular";
+  font-size: 70px;
+  color: #617f6b;
+}
+
+.cont {
+  background-image: url("assets/fond.avif");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position-x: 50%;
+}
+
+.cont2 {
+  background-color: #e8f0eb;
+}
+
+.img2 {
+  border-radius: 7px;
+}
+
+@media (min-width: 320px) and (max-width: 374px) {
+  .cont1 {
+    background-image: url("assets/fondo2.jpeg");
+    padding-top: 90%;
+    background-repeat: no-repeat;
+  }
+
+  .banner-principal {
+    padding-top: 12%;
+  }
+
+  .padd {
+    padding-top: 7%;
+  }
+}
+
+@media (min-width: 375px) and (max-width: 424px) {
+  .cont1 {
+    background-image: url("assets/fondo2.jpeg");
+    padding-top: 75%;
+    background-repeat: no-repeat;
+  }
+
+  .banner-principal {
+    padding-top: 15%;
+  }
+
+  .padding-name {
+    padding-top: 13%;
+  }
+
+}
+
+@media (min-width: 425px) and (max-width: 767px) {
+.cont1 {
+    background-image: url("assets/fondo2.jpeg");
+    padding-top: 65%;
+    background-repeat: no-repeat;
+  }
+
+  .banner-principal {
+    padding-top: 13%;
+  }
+
+  .padding-name {
+    padding-top: 10%;
+  }
+
+  .padding-card {
+    padding-left: 15%;
+  }
+}
+
+@media (min-width: 768px) and (max-width: 1024px) {
+  .cont1 {
+    padding-top: 5%;
+  }
+
+  .initialsDate {
+    font-size: 100px;
+  }
+
+  .text-shipping-date {
+    font-size: 30px
+  }
+
+ .text-shipping2 {
+  font-size: 20px
+ }
+
+ .names {
+  font-size: 50px;
+ }
+
+  .text-subtitle {
+    font-size: 25px;
+  }
+
+}
+
+@media (min-width: 1025px) and (max-width: 1280px) {
+}
+
+@media (min-width: 1281px) and (max-width: 1439px) {
+}
+
+@media (min-width: 1440px) and (max-width: 2559px) {
+}
+
+@media (min-width: 2560px) {
+  .cont1 {
+    padding-top: 10%;
+  }
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+<q-page>
+<card-component></card-component>
+</q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import CardComponent from 'components/CardComponent.vue'
+
+export default defineComponent({
+  name: 'PageIndex',
+   components: { CardComponent }
+})
+</script>
